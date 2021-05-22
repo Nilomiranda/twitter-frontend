03f9a333c8e6f05055dc8533aa4298a48a684fc3
Nilomiranda <nilomiranda3@gmail.com>
2021-05-21 21:54:17 -0300

    implemented fuctionality to follow or unfollow users

--- components/user/UserFollowCard.tsx
+++ components/user/UserFollowCard.tsx
@@ -1,7 +1,9 @@
 import { Button, Flex } from '@chakra-ui/react'
-import { useQuery } from 'react-query'
+import { useMutation, useQuery } from 'react-query'
 import { User } from '../../interfaces/user'
 import UserHeader from './UserHeader'
+import { followUser, unfollowUser } from '../../services/follow'
+import { queryClient } from '../../config/queryClient'
 
 interface UserFollowCardProps {
   user: User
@@ -9,9 +11,30 @@ interface UserFollowCardProps {
 
 const UserFollowCard = ({ user }: UserFollowCardProps) => {
   const { data: userData } = useQuery<{ user: User }>('sessions')
-  const { data: followingData } = useQuery<{ following: boolean }>(`following/${userData?.user?.id}/following?id=${user?.id}`, { enabled: !!userData?.user?.id && !!user?.id })
+  const { data: followingData, refetch } = useQuery<{ following: boolean }>(`following/${userData?.user?.id}/following?id=${user?.id}`, { enabled: !!userData?.user?.id && !!user?.id })
 
-  const handleToggleFollowClick = () => {}
+  const followMutation = useMutation(() => followUser(user?.id))
+  const unfollowMutation = useMutation(() => unfollowUser(user?.id))
+
+  const handleUnfollow = async () => unfollowMutation?.mutateAsync()
+
+  const handleFollow = async () => followMutation?.mutateAsync()
+
+  const handleToggleFollowClick = async () => {
+    const { following } = followingData || { following: false }
+
+    try {
+      if (following) {
+        await handleUnfollow()
+      } else {
+        await handleFollow()
+      }
+      refetch()
+      queryClient?.refetchQueries('feed')
+    } catch (err) {
+      console.error('ERROR:: Could not follow or unfollow user', err)
+    }
+  }
 
   return (
     <Flex direction="row" justifyContent="space-between" w="100%">
