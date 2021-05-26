6f87764447efff78cef48aaeeccf53781f1e51b3
Nilomiranda <nilomiranda3@gmail.com>
2021-05-26 16:34:33 -0300

    added with credentials request in authGuard

--- services/session.ts
+++ services/session.ts
@@ -2,21 +2,12 @@ import axios, { AxiosResponse } from 'axios'
 import { httpClient } from '../config/queryClient'
 import { User } from '../interfaces/user'
 
-export const prefetchSession = async (
-  req
-): Promise<AxiosResponse<{ user: User }>> =>
+export const prefetchSession = async (req): Promise<AxiosResponse<{ user: User }>> =>
   axios.get<{ user: User }>('http://localhost:3000/sessions', {
     headers: req?.headers,
+    withCredentials: true,
   })
 
-export const signIn = async ({
-  email,
-  password,
-  nickname,
-}: {
-  email: string
-  password: string
-  nickname: string
-}) => httpClient.post('sessions', { email, password, nickname })
+export const signIn = async ({ email, password, nickname }: { email: string; password: string; nickname: string }) => httpClient.post('sessions', { email, password, nickname })
 
 export const signOut = async () => httpClient.delete('sessions')
