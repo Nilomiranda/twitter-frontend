ae00caf0c214c4c09fabdd04e281d5c9c07a1a98
Nilomiranda <nilomiranda3@gmail.com>
2021-05-28 21:53:29 -0300

    implemented whole flow to update profile

--- services/user.ts
+++ services/user.ts
@@ -8,8 +8,16 @@ export interface SignUpPayload {
   password: string
 }
 
+export interface UpdateProfilePayload {
+  nickname?: string
+  current_password?: string
+  new_password?: string
+  new_password_confirmation?: string
+  profile_picture_url?: string
+}
+
 export const createUser = async (payload: SignUpPayload) => httpClient.post('users', payload)
 
 export const deleteProfilePicture = async (userId: number) => httpClient?.patch(`users/${userId}`, { profile_picture_url: null })
 
-export const updateUserProfile = async (user: User): Promise<AxiosResponse<{ user: User }>> => httpClient?.patch(`users/${user?.id}`, user)
+export const updateUserProfile = async (userId: number, user: UpdateProfilePayload): Promise<AxiosResponse<{ user: User }>> => httpClient?.patch(`users/${userId}`, user)
